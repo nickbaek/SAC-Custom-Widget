8b17f02202b4e633b7dcdd1b9651b755dae759be
nickbaek <sollip1227@naver.com>
2020-02-27 00:30:34 +0900

    static folder location chaged

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-
-
-
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
 
-app.use('/static', express.static(__dirname + '/googleGauge'));
+app.use('/static', express.static(__dirname + '/public/google'));
 
 app.get('/', function(req, res){
     res.send('Hello World');
